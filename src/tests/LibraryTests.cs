cab3ec2ce8f7ed8ccc689c60d5565d9993ba3bf7
Dimenus <ryan.saunderson88@gmail.com>
2022-06-09 10:03:04 -0500

    added SyncFusion as an additional target

--- src/tests/LibraryTests.cs
+++ src/tests/LibraryTests.cs
@@ -1,19 +1,24 @@
 using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using NUnit.Framework;
 using PdfServices.Lib;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Parsing;
 
 namespace PdfServices.Tests
 {
     public class LibraryTests
     {
+        private long _workingSet;
         private ZMuPdfLib _mLibContext = null!;
         
         [SetUp]
         public void Setup()
         {
+            _workingSet = Process.GetCurrentProcess().PeakWorkingSet64;
             _mLibContext = ZMuPdfLib.Create();
             Directory.CreateDirectory("outputs");
         }
@@ -23,20 +28,25 @@ namespace PdfServices.Tests
         {
             _mLibContext.Dispose();
         }
-
-        [TestCase(new [] { "foo", "bar"}, "output")]
+        
+        [TestCase(new [] { "samples/combineFilesInput1.pdf", "samples/combineFilesInput2.pdf"}, "output")]
         public void CreateOutput(string[] inputFiles, string outputPath)
         {
             var stopwatch = Stopwatch.StartNew();
             _mLibContext.OpenOutput();
-            AddSampleFile("samples/combineFilesInput1.pdf");
-            AddSampleFile("samples/combineFilesInput2.pdf");
+            foreach (var item in inputFiles) {
+                //Get Input Index from Lib
+                //Graft 
+                AddSampleFile(item);
+            }
             var returned_bytes = _mLibContext.CombineOutput();
+            var memory_diff = Process.GetCurrentProcess().PeakWorkingSet64 - _workingSet;
+            Console.WriteLine($"working_set_diff ({memory_diff:N0}), processing time => {stopwatch.Elapsed}");
+
             using var sw = File.Open("outputs/samples.pdf", FileMode.Create);
             sw.Write(returned_bytes);
             sw.Flush();
             sw.Close();
-            Console.WriteLine($"processing time => {stopwatch.Elapsed}");
             _mLibContext.DropOutput();
         }
         
@@ -45,14 +55,17 @@ namespace PdfServices.Tests
         {
             var stopwatch = Stopwatch.StartNew();
             _mLibContext.OpenOutput();
-            AddSampleFile("samples/Workflow_FEP5.pdf");
-            AddSampleFile("samples/Workview_FEP5.pdf");
+            foreach (var item in Directory.GetFiles("samples/", "*.pdf")) {
+                AddSampleFile(item);
+            }
             var returned_bytes = _mLibContext.CombineOutput();
+            var memory_diff = Process.GetCurrentProcess().PeakWorkingSet64 - _workingSet;
+            Console.WriteLine($"working_set_diff ({memory_diff:N0}), processing time => {stopwatch.Elapsed}");
+            
             using var sw = File.Open("outputs/large_sample.pdf", FileMode.Create);
             sw.Write(returned_bytes);
             sw.Flush();
             sw.Close();
-            Console.WriteLine($"processing time => {stopwatch.Elapsed}");
             _mLibContext.DropOutput();
         }
         
@@ -64,12 +77,60 @@ namespace PdfServices.Tests
             AddPartialSampleFile("samples/combineFilesInput1.pdf", 2, 2);
             AddSampleFile("samples/combineFilesInput2.pdf");
             var returned_bytes = _mLibContext.CombineOutput();
+            var memory_diff = Process.GetCurrentProcess().PeakWorkingSet64 - _workingSet;
+            Console.WriteLine($"working_set_diff ({memory_diff:N0}), processing time => {stopwatch.Elapsed}");
+            
             using var sw = File.Open("outputs/partial_samples.pdf", FileMode.Create);
             sw.Write(returned_bytes);
             sw.Flush();
             sw.Close();
-            Console.WriteLine($"processing time => {stopwatch.Elapsed}");
             _mLibContext.DropOutput();
+        }
+
+        [TestCase("samples/combineFilesInput1.pdf", "samples/combineFilesInput2.pdf")]
+        [TestCase(
+            "samples/combineFilesInput1.pdf",
+            "samples/combineFilesInput2.pdf",
+            "samples/combineFilesInput1.pdf"
+        )]
+        public void SyncFusionCreateOutput(params string[] inputs)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            using var output_doc = new PdfDocument();
+            
+            foreach (var input_file in inputs) {
+                var file_bytes = File.ReadAllBytes(input_file);
+                var loaded_doc = new PdfLoadedDocument(file_bytes);
+                PdfDocumentBase.Merge(output_doc, loaded_doc);
+                loaded_doc.Close();
+            }
+            
+            var memory_diff = Process.GetCurrentProcess().PeakWorkingSet64 - _workingSet;
+            Console.WriteLine($"working_set_diff ({memory_diff:N0}), processing time => {stopwatch.Elapsed}");
+            using var output_stream = new FileStream("outputs/sync_fusion_sample.pdf", FileMode.Create);
+            output_doc.Save(output_stream);
+            output_stream.Flush();
+        }
+
+        [Test]
+        public void SyncFusionCreateLargeOutput()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            using var output_doc = new PdfDocument();
+
+            var file_list = Directory.GetFiles("samples/", "*.pdf");
+            foreach (var item in file_list) {
+                var file_bytes = File.ReadAllBytes(item);
+                var loaded_doc = new PdfLoadedDocument(file_bytes);
+                PdfDocumentBase.Merge(output_doc, loaded_doc);
+                loaded_doc.Close();
+            }
+
+            var memory_diff = Process.GetCurrentProcess().PeakWorkingSet64 - _workingSet;
+            Console.WriteLine($"working_set_diff ({memory_diff:N0}), processing time => {stopwatch.Elapsed}");
+            using var output_stream = new FileStream("outputs/sync_fusion_sample.pdf", FileMode.Create);
+            output_doc.Save(output_stream);
+            output_stream.Flush();
         }
 
         private void AddSampleFile(string filePath)
